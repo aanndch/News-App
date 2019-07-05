acd346ee451a5773045fc6d0de7a5656319bbc9e
Anand Chembarpu <anandchembarpu@gmail.com>
2019-07-06 00:18:19 +0530

    Minor change to search and updated CSS

--- src/components/ChannelNews.js
+++ src/components/ChannelNews.js
@@ -61,14 +61,90 @@ export default class ChannelNews extends Component {
                     "urlToImage": "https://s.abcnews.com/images/International/WireAP_ae062147cb6945769a977147e693e0ac_16x9_992.jpg",
                     "publishedAt": "2019-07-05T13:26:17Z",
                     "content": "The U.N.'s special envoy for Syria, Geir Pedersen, is focusing his efforts on hostilities in the northwestern province of Idlib and attempts to draft the nation's new constitution.\r\nDuring talks Friday with Russian Foreign Minister Sergey Lavrov, Pedersen voi… [+603 chars]"
+                },
+                {
+                    "source": {
+                        "id": "abc-news",
+                        "name": "ABC News"
+                    },
+                    "author": "The Associated Press",
+                    "title": "Coast Guard searching for man who fell from Carnival ship",
+                    "description": "Get breaking national and world news, broadcast video coverage, and exclusive interviews. Find the top news online at ABC news.",
+                    "url": "https://abcnews.go.com/US/wireStory/coast-guard-searching-man-fell-carnival-ship-64148134",
+                    "urlToImage": null,
+                    "publishedAt": "2019-07-05T13:28:11Z",
+                    "content": "The U.S. Coast Guard is searching for a 37-year-old man who apparently fell overboard from the cruise ship Carnival Victory.\r\nThe agency says it was notified Thursday that a Carnival cruise ship crewmember fell overboard about 30 miles (48 kilometers) northwe… [+148 chars]"
+                    },
+                    {
+                    "source": {
+                        "id": "abc-news",
+                        "name": "ABC News"
+                    },
+                    "author": "The Associated Press",
+                    "title": "UN envoy for Syria visits Moscow for talks",
+                    "description": "The U.N.'s special envoy for Syria, Geir Pedersen, is focusing his efforts on hostilities in the northwestern province of Idlib and attempts to draft the nation's new constitution",
+                    "url": "https://abcnews.go.com/International/wireStory/envoy-syria-visits-moscow-talks-64148076",
+                    "urlToImage": "https://s.abcnews.com/images/International/WireAP_ae062147cb6945769a977147e693e0ac_16x9_992.jpg",
+                    "publishedAt": "2019-07-05T13:26:17Z",
+                    "content": "The U.N.'s special envoy for Syria, Geir Pedersen, is focusing his efforts on hostilities in the northwestern province of Idlib and attempts to draft the nation's new constitution.\r\nDuring talks Friday with Russian Foreign Minister Sergey Lavrov, Pedersen voi… [+603 chars]"
+                },
+                {
+                    "source": {
+                        "id": "abc-news",
+                        "name": "ABC News"
+                    },
+                    "author": "The Associated Press",
+                    "title": "Coast Guard searching for man who fell from Carnival ship",
+                    "description": "Get breaking national and world news, broadcast video coverage, and exclusive interviews. Find the top news online at ABC news.",
+                    "url": "https://abcnews.go.com/US/wireStory/coast-guard-searching-man-fell-carnival-ship-64148134",
+                    "urlToImage": null,
+                    "publishedAt": "2019-07-05T13:28:11Z",
+                    "content": "The U.S. Coast Guard is searching for a 37-year-old man who apparently fell overboard from the cruise ship Carnival Victory.\r\nThe agency says it was notified Thursday that a Carnival cruise ship crewmember fell overboard about 30 miles (48 kilometers) northwe… [+148 chars]"
+                    },
+                    {
+                    "source": {
+                        "id": "abc-news",
+                        "name": "ABC News"
+                    },
+                    "author": "The Associated Press",
+                    "title": "UN envoy for Syria visits Moscow for talks",
+                    "description": "The U.N.'s special envoy for Syria, Geir Pedersen, is focusing his efforts on hostilities in the northwestern province of Idlib and attempts to draft the nation's new constitution",
+                    "url": "https://abcnews.go.com/International/wireStory/envoy-syria-visits-moscow-talks-64148076",
+                    "urlToImage": "https://s.abcnews.com/images/International/WireAP_ae062147cb6945769a977147e693e0ac_16x9_992.jpg",
+                    "publishedAt": "2019-07-05T13:26:17Z",
+                    "content": "The U.N.'s special envoy for Syria, Geir Pedersen, is focusing his efforts on hostilities in the northwestern province of Idlib and attempts to draft the nation's new constitution.\r\nDuring talks Friday with Russian Foreign Minister Sergey Lavrov, Pedersen voi… [+603 chars]"
+                },
+                {
+                    "source": {
+                        "id": "abc-news",
+                        "name": "ABC News"
+                    },
+                    "author": "The Associated Press",
+                    "title": "Coast Guard searching for man who fell from Carnival ship",
+                    "description": "Get breaking national and world news, broadcast video coverage, and exclusive interviews. Find the top news online at ABC news.",
+                    "url": "https://abcnews.go.com/US/wireStory/coast-guard-searching-man-fell-carnival-ship-64148134",
+                    "urlToImage": null,
+                    "publishedAt": "2019-07-05T13:28:11Z",
+                    "content": "The U.S. Coast Guard is searching for a 37-year-old man who apparently fell overboard from the cruise ship Carnival Victory.\r\nThe agency says it was notified Thursday that a Carnival cruise ship crewmember fell overboard about 30 miles (48 kilometers) northwe… [+148 chars]"
+                    },
+                    {
+                    "source": {
+                        "id": "abc-news",
+                        "name": "ABC News"
+                    },
+                    "author": "The Associated Press",
+                    "title": "UN envoy for Syria visits Moscow for talks",
+                    "description": "The U.N.'s special envoy for Syria, Geir Pedersen, is focusing his efforts on hostilities in the northwestern province of Idlib and attempts to draft the nation's new constitution",
+                    "url": "https://abcnews.go.com/International/wireStory/envoy-syria-visits-moscow-talks-64148076",
+                    "urlToImage": "https://s.abcnews.com/images/International/WireAP_ae062147cb6945769a977147e693e0ac_16x9_992.jpg",
+                    "publishedAt": "2019-07-05T13:26:17Z",
+                    "content": "The U.N.'s special envoy for Syria, Geir Pedersen, is focusing his efforts on hostilities in the northwestern province of Idlib and attempts to draft the nation's new constitution.\r\nDuring talks Friday with Russian Foreign Minister Sergey Lavrov, Pedersen voi… [+603 chars]"
                 }]
             };
 
         const articles = data.articles.map(article => {
             return {title: article.title, description: article.description };
         })
-
-        console.log(articles);
 
         this.setState({loading: false, articles: articles});
 
@@ -82,29 +158,29 @@ export default class ChannelNews extends Component {
 
         let articles = [];
         articles = this.state.articles.filter(article => {
-            return article.title.toLowerCase().includes(this.state.search);
+            return article.title.toLowerCase().includes(this.state.search.toLowerCase());
         })
-
-        console.log(articles);
 
         if (this.state.loading) {
             articles = <div>Loading GIF</div>
         } else {
             articles = articles.map((article, i) => {
                 return (<div key={i} className="article-card">
-                            <div>{article.title}</div>
-                            <div>{article.description}</div>
+                            <div className="article-title">{article.title}</div>
+                            <div className="article-desc">{article.description}</div>
                         </div>);
             })
         }
 
         return (
             <div>
-                <div>
-                    <input type='text' name='articleSearch' onChange={this.handleSearch} placeholder='market' maxLength='30'/>
+                <div id="article-search">
+                    <input type='text' name='articleSearch' onChange={this.handleSearch} placeholder='Search' maxLength='30' id="search-bar"/>
                     {/* <div>Search Icon</div>  */}
                 </div>
-                {articles}
+                <div className="container">
+                    {articles}
+                </div>
             </div>
         )
     }
