629351fe5d9ee3d26c0cd211a13af17e724409bd
Anand Chembarpu <anandchembarpu@gmail.com>
2019-07-07 21:41:14 +0530

    Changed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
   return (
     <div id="universal-footer">
-      Powered by newsapi.org
+      <div id="footer-left">
+        Made by
+        <a href="https://github.com/aanndch" id="github-handle">
+          Anand
+        </a>
+      </div>
+      <div id="footer-right">
+        Powered by
+        <a href="https://newsapi.org/" id="api-link">
+          newsapi.org
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Footer;
